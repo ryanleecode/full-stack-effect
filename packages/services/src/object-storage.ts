0656c5cfd49b7e4b4d809e3a4835d6de9e3c5808
Ryan Lee <drdgvhbh@gmail.com>
2024-04-12 19:05:57 -0400

    feat: aws object storage

---
+++ packages/services/src/object-storage.ts
@@ -0,0 +1,12 @@
+import { type Effect } from 'effect';
+import { type UnknownException } from 'effect/Cause';
+
+namespace ObjectStorageClient {
+	export declare function getObject(key: string, transform: 'string'): Effect.Effect<string, UnknownException, never>;
+	export declare function getObject(
+		key: string,
+		transform: 'binary',
+	): Effect.Effect<Uint8Array, UnknownException, never>;
+}
+
+export type ObjectStorageClient = typeof ObjectStorageClient;
